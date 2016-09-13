3560c835e70e94a68bb171447997c05f696d7772
robotkutya <mihalyfuredi@gmail.com>
2016-09-13 09:57:08 +0200

    ContactList in progress

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 
 class Contact:
-    pass
+    all_contacts = []
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.all_contacts.append(self)
+
+    @classmethod
+    def reset_contacts(cls):
+        cls.all_contacts = []
+
+
+class Supplier(Contact):
+    all_orders = dict()
+
+    def order(self, string):
+        all_orders[self.email] = string
+
+
+class ContactList(Contact):
+
+    def search(self, string):
+        for contact in self.all_contacts:
+
